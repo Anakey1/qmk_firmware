0e339116206e4565b2db828671f12dd649589f08
Anakey1 <u0752255@alumni.hud.ac.uk>
2018-02-18 18:24:27 +0000

    Add files via upload

--- keyboards/handwired/Myth/keymaps/default/keymap.c
+++ keyboards/handwired/Myth/keymaps/default/keymap.c
@@ -7,8 +7,8 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _KEYPAD 1
+#define _QW 0
+#define _KP 1
 #define _FN 2
 
 
@@ -35,22 +35,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------------------' `---------------------------'
  */
 
-[_QWERTY] = { 
-	    {KC_CIRC, UK_TILD, KC_UNDS, KC_LCBR, KC_RCBR,        KC_LBRC,        KC_RBRC, KC_LABK, KC_RABK,            KC_LPRN,            KC_RPRN, KC_AMPR, UK_BSLS, UK_PIPE, KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_ESC,  KC_Q,	   KC_W,    KC_E,    KC_R,           KC_T,           KC_Y,    KC_U,    KC_I,               KC_O,               KC_P,    KC_GRV,  UK_AT,   KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_TAB,  KC_A,	   KC_S,    KC_D,    KC_F,           KC_G,           KC_H,    KC_J,    KC_K,               KC_L,               UK_QUOT, KC_COLN, KC_SCLN, KC_DEL,  KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_LSFT, KC_Z,	   KC_X,    KC_C,    KC_V,           KC_B,           KC_N,    KC_M,    KC_EXLM,            KC_QUES,            KC_COMM, KC_QUOT, KC_UP,   KC_ENT,  KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI, LT(FN, KC_SPC), LT(FN, KC_SPC), KC_SPC,  KC_SPC,  LT(KEYPAD, KC_SPC), LT(KEYPAD, KC_SPC), KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,              KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   UK_PND,  UK_HASH, KC_PERC, KC_DLR }, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,              KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_PLUS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,              KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,    KC_MINS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,              KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_ASTR}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_NO,          KC_NO,   KC_NO,   KC_NO,              KC_NO,              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_0,    KC_DOT,  KC_EQL,  KC_SLSH}
-},
+[_QW] = KEYMAP(
+	    KC_CIRC, UK_TILD, KC_UNDS, KC_LCBR, KC_RCBR, KC_LBRC,       KC_RBRC, KC_LABK, KC_RABK, KC_LPRN,       KC_RPRN, KC_AMPR, UK_BSLS, UK_PIPE,  UK_PND,  UK_HASH, KC_PERC, KC_DLR, \
+		KC_ESC,  KC_Q,	  KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,          KC_P,    KC_GRV,  UK_AT,   KC_BSPC,  KC_7,    KC_8,    KC_9,    KC_PLUS,\
+		KC_TAB,  KC_A,	  KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,          UK_QUOT, KC_COLN, KC_SCLN, KC_DEL,   KC_4,    KC_5,    KC_6,    KC_MINS,\
+		KC_LSFT, KC_Z,	  KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_EXLM, KC_QUES,       KC_COMM, KC_QUOT, KC_UP,   KC_ENT,   KC_1,    KC_2,    KC_3,    KC_ASTR,\
+		KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI,          LT(1, KC_SPC),          KC_SPC,           LT(2, KC_SPC), KC_RSFT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_0,    KC_DOT,  KC_EQL,  KC_SLSH),
 
 /* Keypad
  * ,-------------------------------------------------------------------------------------------------. ,---------------------------.
- * |  ^   |   ~  |   _  |   {  |   }  |   [  |   ]  |   <  |   >  |   (  |   )  |  &   |   \  |  |   | |     |   #  |   %  |  $   |
+ * |  ^   |   ~  |   _  |   {  |   }  |   [  |   ]  |   <  |   >  |   (  |   )  |  &   |   \  |  |   | |      |   #  |   %  |  $   |
  * |------+------+------+------+------+-------------+------+------+------+------+------+------+------| |------+------+------+------|
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  `   |   @  | Bksp | |   7  |   8  |   9  |  +   |
  * |------+------+------+------+------+-------------+------+------+------+------+------+------+------| |------+------+------+------|
@@ -62,18 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------------------' `---------------------------'
  */
 
- [_KEYPAD] = {
-		{KC_CIRC, UK_TILD, KC_UNDS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_LPRN, KC_RPRN, KC_AMPR, UK_BSLS, BACKLIT, KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_ESC,  KC_Q,	   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  UK_AT,   KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_TAB,  KC_A,	   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    UK_QUOT, KC_COLN, KC_SCLN, KC_DEL,  KC_NO,   KC_NO,   KC_NO,   KC_NO  }, 
-		{KC_LSFT, KC_Z,	   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_EXLM, KC_QUES, KC_PAUS, KC_PSCR, KC_PGUP, KC_ENT,  KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_RSFT, KC_HOME, KC_PGDN, KC_END,  KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   UK_EURO, UK_HASH, KC_PERC, KC_DLR }, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PLUS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_MINS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_ASTR}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,   KC_DOT,  KC_EQL,  KC_SLSH}
-},
+[_KP] = KEYMAP(
+		KC_CIRC, UK_TILD, KC_UNDS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_LPRN, KC_RPRN, KC_AMPR, UK_BSLS, BACKLIT, UK_EURO, UK_HASH, KC_PERC, KC_DLR ,\
+		KC_ESC,  KC_Q,	  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  UK_AT,   KC_BSPC, KC_P7,   KC_P8,   KC_P9,   KC_PLUS,\
+		KC_TAB,  KC_A,	  KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    UK_QUOT, KC_COLN, KC_SCLN, KC_DEL,  KC_P4,   KC_P5,   KC_P6,   KC_MINS,\
+		KC_LSFT, KC_Z,	  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_EXLM, KC_QUES, KC_PAUS, KC_PSCR, KC_PGUP, KC_ENT,  KC_P1,   KC_P2,   KC_P3,   KC_ASTR,\
+		KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI,          KC_TRNS,          KC_SPC,           KC_TRNS, KC_RSFT, KC_HOME, KC_PGDN, KC_END,  KC_P0,   KC_DOT,  KC_EQL,  KC_SLSH),
 
 /* Fn
  * ,-------------------------------------------------------------------------------------------------. ,---------------------------.
@@ -89,22 +77,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------------------' `---------------------------'
  */
 
-[_FN] = {
-		{KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  UK_BSLS, UK_PIPE, KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  UK_AT,   KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_TAB,  KC_A,	   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    UK_QUOT, KC_COLN, KC_SCLN, KC_DEL , KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_LSFT, KC_Z,	   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_EXLM, KC_QUES, KC_COMM, KC_QUOT, KC_UP,   KC_ENT , KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO  },
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   UK_PND,  UK_HASH, KC_PERC, KC_DLR }, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_PLUS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,    KC_MINS}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_ASTR}, 
-		{KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_0,    KC_DOT,  KC_EQL,  KC_SLSH}
-}
+[_FN] = KEYMAP(
+		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  UK_BSLS, UK_PIPE, UK_PND,  UK_HASH, KC_PERC, KC_DLR ,\
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  UK_AT,   KC_BSPC, KC_7,    KC_8,    KC_9,    KC_PLUS,\
+		KC_TAB,  KC_A,	  KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    UK_QUOT, KC_COLN, KC_SCLN, KC_DEL,  KC_4,    KC_5,    KC_6,    KC_MINS,\
+		KC_LSFT, KC_Z,	  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_EXLM, KC_QUES, KC_COMM, KC_QUOT, KC_UP,   KC_ENT,  KC_1,    KC_2,    KC_3,    KC_ASTR,\
+		KC_LCTL, KC_LCAP, KC_LALT, KC_LGUI,          KC_TRNS,          KC_SPC,           KC_TRNS, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_0,    KC_DOT,  KC_EQL,  KC_SLSH),
 
 };
-
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
